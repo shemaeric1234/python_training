a5ea3b31b99aa9760fe8dbe3cd25abc8015cb64c
SHEMA Eric <47531860+shemaeric1234@users.noreply.github.com>
2022-07-20 17:02:48 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-#number1= int(input('inter number 1:'))
-#number2= int(input('inter number 2:'))
-#number3= int(input('inter number 3:'))
-#print('sum of {},{} and {} is: {}'.\
-     # format(number1,number1,number1,(number1\
-       #                               +number2+number3)))
+# number1= int(input('inter number 1:'))
+# number2= int(input('inter number 2:'))
+# number3= int(input('inter number 3:'))
+# print('sum of {},{} and {} is: {}'.\
+# format(number1,number1,number1,(number1\
+#                               +number2+number3)))
 
-#name="shema ERIC"
+# name="shema ERIC"
 
-#for n in range(2,15,2):
- #   print(n)
-#else:
- #   print('done !!!!')
+# for n in range(2,15,2):
+#   print(n)
+# else:
+#   print('done !!!!')
 
 
 """
@@ -20,8 +20,6 @@ userName = input("inter you name:")
 for n in userName:
     print(n, '*', end='')
 """
-
-
 
 """
 # 2 sum  of 10 number
@@ -33,14 +31,12 @@ else:
    print("the sum of first 10 number is:{}".format(mySum))
 """
 
-
 """
 # 3 odd / even
 print('odd numbers:', end='\t')
 for i in range(1, 100):
     print(i, end=',')
 """
-
 
 """
 #4 divisible by 7 and multiple of 5 btn 500-1400
@@ -52,7 +48,6 @@ for i in range(499,1401):
 print(count) 
 """
 
-
 """
 #5 not divisible by 5
 
@@ -63,9 +58,7 @@ for i in range(1,101):
         print(i, end=",")
 """
 
-
-
-#print("\"he said that \"Kigali is Beautiful\"\"")
+# print("\"he said that \"Kigali is Beautiful\"\"")
 
 """
 word1 = list(input('Enter the word:'))
@@ -117,63 +110,64 @@ for i in numbers:
 print('the sum is:{}'.format(sum1))
 """
 
-#1 sum of all items in  list
+# 1 sum of all items in  list
 
 list1 = [1, 2, 3]
 total = 0
 for n in list1:
     total += n
-#print(total)
+# print(total)
 
 
-#2 largest in list
-list2 = [1, 2,3,6,8,9,10]
+# 2 largest in list
+list2 = [1, 2, 3, 6, 8, 9, 10]
 largest = list2[0]
 for n in list2:
     if largest < n:
         largest = n
-#print(largest)
+# print(largest)
 
-#3 find average in list
+# 3 find average in list
 
 list3 = [1, 2, 3]
 total = 0
 for n in list1:
     total += n
- #print(total/len(list3))
+# print(total/len(list3))
 
-#4 total number of integer in list
+# 4 total number of integer in list
 
 list4 = [1.5, 2, 3, 3.4, 1]
 totalInt = 0
 for n in list4:
     if type(n) == int:
         totalInt += 1
-#print(totalInt)
+# print(totalInt)
 
-#5 find odd/ enven using
+# 5 find odd/ enven using
 
 list5 = [1.5, 2, 3, 3.4, 1]
 
 oddNumber = [n for n in list5 if n % 2 != 0]
 evenNumber = [n for n in list5 if n % 2 == 0]
-#print('odd:{}'.format(oddNumber))
-#print('even:{}'.format(evenNumber))
+# print('odd:{}'.format(oddNumber))
+# print('even:{}'.format(evenNumber))
 
-#6 reverse list using slicing
+# 6 reverse list using slicing
 
 list6 = [1.5, 2, 3, 3.4, 1]
 
-#print(list6[-1: :-1])
+# print(list6[-1: :-1])
 
-#7 remove duplicate in a list
+# 7 remove duplicate in a list
 
-list7 = [10,10,10,20,20,30,40,40,50,70,90]
+list7 = [10, 10, 10, 20, 20, 30, 40, 40, 50, 70, 90]
 result = set(list7)
-#print(result)
+# print(result)
 
-#8 print midle word or two middle words
+# 8 print midle word or two middle words
 
+"""
 sentence = input("inter the sentence you want:")
 sentenceList = sentence.split(" ")
 length = len(sentenceList)
@@ -181,9 +175,123 @@ if length % 2 == 0:
     print(sentenceList[int(length/2)-1] + " " + sentenceList[int(length/2)])
 else:
     print(sentenceList[int(length / 2)])
+"""
+
+# unpacking a list of a tuple
+list9 = [10, 10, 10, 20, 20, 30, 40, 40, 50, 70, 90]
+# [...a,b]=list9
+*a, b = list9
+
+# print(b)
+
+dct = {'name': 'shema', 'age': 12}
+value = 'name'
+
+# print(dct.get(value))
 
 
+# dictionary
+# print(range(0,0.0))
+# command line argument
+
+"""
+in terminal we use  python main.py 10 20
+import sys
+
+print(sys.argv[1])
+print(sys.argv[0]) to display script name
+"""
+
+"""
+import sys as sys1
+
+print(sys1.argv[1])
+print(sys1.argv[0])
+"""
+
+# 1 guess game
 
 
+import random
 
+reward = {
+    '1': 10000,
+    '2': 5000,
+    '3': 3000,
+    '4': 2000,
+    '5': 1000
+}
+count = 6
+for i in count:
+    userNumber = int(input('guess number between 1 and 10:'))
+    rand = random.randrange(1,10)
+    print('system number is:{}'.format(rand[i]))
+    if userNumber == rand:
+        print('congratulation you win {} !!!!'.format(reward[count]))
+        break
+    else:
+        print('Oops ! you fail')
+        continue
+
+
+# 2 calculating area of circle and volume of cone and sylinder
+
+"""
+circle = π · r2
+volume of cylinder = π · r2 · h
+volume of cone = (1/3) · π · r2 · h
+"""
+
+import math
+
+#radius = int(input('inter radius:'))
+#height = int(input('inter height:'))
+
+
+def circle_area(radius):
+    print('circle area is:{}'.format(math.pi * (radius ** 2)))
+
+
+def cylinder_volume(radius, height):
+    print('cylinder volume is:{}'.format(math.pi * (radius ** 2) * height))
+
+
+def cone_volume(radius, height):
+    print('cone volume is:{}'.format((1 / 3) * math.pi * (radius ** 2) * height))
+
+"""
+circle_area(radius)
+cylinder_volume(radius, height)
+cone_volume(radius, height)
+"""
+
+
+#3 state name as input
+
+distr_dic = {
+    'gasabo': {
+        'CNname': 'gasabo cn',
+        'capital': 'capit1',
+        'population': 1003500
+    },
+    'kicukiro': {
+            'CN_name': 'kicukiro cn',
+            'capital': 'kicukiro_capital',
+            'population': 1004500
+        },
+    'nyarugenge': {
+                'CN_name': 'nyarugenge cn',
+                'capital': 'nyarugenge_capital',
+                'population': 10023456
+            },
+}
+
+dist_name = input('enter district name:')
+
+def find_details(dist_name):
+    print(distr_dic[dist_name])
+
+find_details(dist_name)
+
+#4 compare time of execution between factorial of 899 and recursion
 
